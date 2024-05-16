44e1a2c0bfac94883fe4aa884e9961bd92ec543d
lennardevertz <levertz@idriss.xyz>
2024-05-16 18:43:36 +0200

    - add CORS

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import requests
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route('/')
 def hello_world():
@@ -21,8 +24,13 @@ def verify():
         response.raise_for_status()
     except requests.RequestException as e:
         return jsonify({'error': str(e)}), 400
-
-    return jsonify(response.json())
-
+    
+    response = jsonify(response.json())
+    if 'Origin' in request.headers and request.headers['Origin'] == 'null':
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Methods', 'GET')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    return response
+    
 if __name__ == '__main__':
     app.run(debug=True)
