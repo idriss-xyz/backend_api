dfcf0bd2de2d730600eaaa1f54040d664d76fc1c
lennardevertz <levertz@idriss.xyz>
2024-05-17 12:36:15 +0200

    - add request limiter

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 import requests
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
+
+limiter = Limiter(
+    get_remote_address,
+    storage_uri="memory://"
+)
+
+limiter.init_app(app)
 
 
 @app.route('/')
@@ -13,7 +22,7 @@ def hello_world():
 
 
 @app.route('/get-connected-addresses', methods=['GET'])
-def verify():
+def get_fc():
     fid = request.args.get('fid')
     
     if not fid:
@@ -28,6 +37,30 @@ def verify():
         return jsonify({'error': str(e)}), 400
     
     return jsonify(response.json())
+
+
+@app.route('/get-twitter-id', methods=['GET'])
+@limiter.limit("30 per minute")
+def get_twitter():
+    identifier = request.args.get('identifier').replace('@', '')
+    
+    if not identifier:
+        return jsonify({'error': 'Missing identifier parameter'}), 400
+
+    url = f"https://www.idriss.xyz/v1/getTwitterIDPlugin?usernames={identifier}"
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+    except requests.RequestException as e:
+        return jsonify({'error': str(e)}), 400
+    print(response.json())
+    response_json = response.json()
+    if response_json['twitterIDs'] == {}:
+        return jsonify({'error': 'Twitter name not found'}), 400
+    formatted_response = {'id': response_json['twitterIDs'][identifier]}
+    print(formatted_response)
+    return jsonify(formatted_response), 200
     
 if __name__ == '__main__':
     app.run(debug=True)
