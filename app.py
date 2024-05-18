e1d72fe3dec234db1f85e75a41b633b31e6d81a6
lennardevertz <levertz@idriss.xyz>
2024-05-18 21:52:42 +0200

    - add 0x token pricing api

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
+from utils.constants import PRICING_API_URL
+
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -78,9 +80,44 @@ def get_ud():
         response.raise_for_status()
     except requests.RequestException as e:
         return jsonify({'error': str(e)}), 400
-    print(response.json())
     response_json = response.json()
     return jsonify(response_json), 200
+
+
+# Add additional headers for extension context
+@app.route('/token-price', methods=['GET', 'OPTIONS'])
+@limiter.limit("10 per minute")
+def get_token_price():
+    if request.method == "OPTIONS":
+        response = jsonify({"message": "ok"}), 200
+        response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        return response
+    sell_token = request.args.get("sellToken", None)
+    buy_token = request.args.get("buyToken", None)
+    sell_amount = request.args.get("sellAmount", None)
+    network = request.args.get("network", "1")
+
+    if not sell_token or not buy_token or not sell_amount:
+        response = jsonify({"error": "Missing required parameters"}), 400
+        return response
+
+    api_key = os.getenv("API_KEY_0X")
+    url = f"{PRICING_API_URL[network]}/swap/v1/price?sellToken={sell_token}&buyToken={buy_token}&sellAmount={sell_amount}"
+    headers = {"0x-api-key": api_key}
+
+    try:
+        api_response = requests.get(url, headers=headers, timeout=10)
+        api_response.raise_for_status()  # Will raise HTTPError for bad requests
+        response = jsonify(api_response.json()), api_response.status_code
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        return response
+    except requests.RequestException as e:
+        status_code = e.response.status_code if hasattr(e, "response") else 400
+        response = jsonify({"error": str(e)}), status_code
+        response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+        return response
+    
     
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ utils/constants.py
@@ -0,0 +1,8 @@
+PRICING_API_URL = {
+    "1": "https://api.0x.org",
+    "10": "https://optimism.api.0x.org",
+    "56": "https://bsc.api.0x.org",
+    "137": "https://polygon.api.0x.org",
+    "8453": "https://base.api.0x.org",
+    "42161": "https://arbitrum.api.0x.org"
+}
