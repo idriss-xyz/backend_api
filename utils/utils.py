a5db6a8356a8c864034ef01361d5f13b395cc252
lennardevertz <levertz@idriss.xyz>
2024-07-26 17:06:19 +0200

    - add new requirements

--- utils/utils.py
+++ utils/utils.py
@@ -2,12 +2,10 @@ import base64
 from io import BytesIO
 
 import requests
+from PIL import Image
 
 from utils.constants import DEFAULT_NETWORK, TOKEN_ROUTE
 from utils.responses import HTTP_NOT_FOUND, HTTP_OK, create_response
-
-# from PIL import Image
-
 
 
 def get_token_router(network, buy_token, sell_token):
@@ -24,14 +22,14 @@ def fetch_data(url, return_type):
     if return_type == "text":
         response.raise_for_status()
         return create_response({"text": response.text}, HTTP_OK)
-    # if return_type == "blob":
-    #     response.raise_for_status()
-    #     content_type = response.headers.get("content-type", "image/jpeg")
-    #     image = Image.open(BytesIO(response.content))
-    #     buffered = BytesIO()
-    #     image_format = image.format if image.format else "JPEG"
-    #     image.save(buffered, format=image_format)
-    #     img_base64 = base64.b64encode(buffered.getvalue()).decode()
-    #     data_url = f"data:{content_type};base64,{img_base64}"
-    #     return create_response({"image": data_url}, HTTP_OK)
+    if return_type == "blob":
+        response.raise_for_status()
+        content_type = response.headers.get("content-type", "image/jpeg")
+        image = Image.open(BytesIO(response.content))
+        buffered = BytesIO()
+        image_format = image.format if image.format else "JPEG"
+        image.save(buffered, format=image_format)
+        img_base64 = base64.b64encode(buffered.getvalue()).decode()
+        data_url = f"data:{content_type};base64,{img_base64}"
+        return create_response({"image": data_url}, HTTP_OK)
     return create_response({}, HTTP_NOT_FOUND)
