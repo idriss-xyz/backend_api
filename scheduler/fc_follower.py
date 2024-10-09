a4bdc719f864976193f533af7bc898e1af7c473c
lennardevertz <levertz@idriss.xyz>
2024-10-09 20:37:24 +0200

    fix: scheduler task

--- scheduler/fc_follower.py
+++ scheduler/fc_follower.py
@@ -2,7 +2,20 @@
 Periodically sends requests to update @idriss farcaster followers.
 """
 
+import time
+
 import requests
 
-eligible_addresses = requests.get("https://www.api.idriss.xyz/update-fc_follower", timeout=600)
+url = "https://www.api.idriss.xyz/update-fc-follower"
+
+for _ in range(4):
+    try:
+        eligible_addresses = requests.get(url, timeout=100)
+        print("Request sent. Status code:", eligible_addresses.status_code)
+
+    except requests.RequestException as e:
+        print("Error sending request:", str(e))
+
+    time.sleep(120)
+
 
